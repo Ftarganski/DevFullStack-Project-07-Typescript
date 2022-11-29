d1a0f9ebd8954b5f6189ed3c9e34293d71b0e08d
Francis Targanski <francis@targanski.com>
2022-11-29 07:37:49 -0300

    adjusting css and ux

--- genius/build/assets/js/script.js
+++ genius/build/assets/js/script.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("xcvxcvxcvx");
+var jogoIniciado = false;
+const btnStart = document.getElementById('part-4');
+const defineHeight = () => {
+    const elements = document.querySelectorAll(".genius__item");
+    elements.forEach((element) => {
+        element.style.height = `${element.clientWidth}px`;
+    });
+};
+const startGame = () => {
+    if (!jogoIniciado) {
+        alert("Go!");
+        jogoIniciado = true;
+        if (btnStart) {
+            btnStart.disabled = true;
+            btnStart.style.cursor = "none";
+            btnStart.style.color = "black";
+        }
+    }
+};
+defineHeight();
+window.addEventListener("resize", () => {
+    defineHeight();
+});
--- genius/src/script.ts
+++ genius/src/script.ts
@@ -1,1 +1,32 @@
-console.log("xcvxcvxcvx")
+var jogoIniciado = false;
+
+const btnStart = document.getElementById('part-4') as HTMLButtonElement | null;
+
+const defineHeight = () => {
+    const elements: any = document.querySelectorAll(".genius__item");
+    elements.forEach((element: any) => {
+        element.style.height = `${element.clientWidth}px`
+    })
+}
+
+const startGame = () => {
+    if (!jogoIniciado) {
+        alert("Go!");
+        jogoIniciado = true;
+        if (btnStart) {
+            btnStart.disabled = true;
+            btnStart.style.cursor = "none";
+            btnStart.style.color = "black";
+        }
+    }
+}
+
+defineHeight();
+
+window.addEventListener("resize", () => {
+    defineHeight();
+})
+
+
+
+
