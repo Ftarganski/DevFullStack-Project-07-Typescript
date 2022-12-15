ab4a394feae48d1c7793d9f1e3b8c7643ea94c7b
Francis Targanski <francis@targanski.com>
2022-12-15 17:59:33 -0300

    insert game logic

--- genius/build/assets/js/script.js
+++ genius/build/assets/js/script.js
@@ -1,24 +1,112 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var jogoIniciado = false;
 const btnStart = document.getElementById('part-4');
+const parts = [
+    document.getElementById('part-0'),
+    document.getElementById('part-1'),
+    document.getElementById('part-2'),
+    document.getElementById('part-3'),
+    document.getElementById('part-4'),
+];
+const sounds = [
+    new Audio('../build/assets/sounds/sound0.wav'),
+    new Audio('../build/assets/sounds/sound1.wav'),
+    new Audio('../build/assets/sounds/sound2.wav'),
+    new Audio('../build/assets/sounds/sound3.wav'),
+    new Audio('../build/assets/sounds/erro.mp3'),
+];
+const score = document.getElementById("genius__score");
+const areaeError = document.getElementById("area__error");
+let positions = [], mPositions = [];
+const configs = () => {
+    parts.map((part, idx) => {
+        part === null || part === void 0 ? void 0 : part.addEventListener('click', (event) => setPosition(event));
+    });
+};
+const startGame = () => __awaiter(void 0, void 0, void 0, function* () {
+    if (!jogoIniciado) {
+        jogoIniciado = true;
+        if (btnStart) {
+            btnStart.disabled = true;
+            btnStart.innerHTML = "GAME </br> STARTED";
+            btnStart.style.fontSize = "1.5em";
+            btnStart.style.cursor = "none";
+            loadPosition();
+        }
+    }
+    // GAME POSITION GENERATION LOGIC
+});
+const loadPosition = () => __awaiter(void 0, void 0, void 0, function* () {
+    let aleatory = 0;
+    if (positions.length >= 4) {
+        aleatory = Math.floor(Math.random() * 4);
+    }
+    else {
+        if (positions.length > 0) {
+            aleatory = positions[positions.length - 1] + 1;
+        }
+    }
+    positions.push(aleatory);
+    yield iluminatePosition();
+    mPositions = [];
+});
+const setPosition = (event) => __awaiter(void 0, void 0, void 0, function* () {
+    let element = event.currentTarget.getAttribute(`source`);
+});
+const iluminatePosition = () => __awaiter(void 0, void 0, void 0, function* () {
+    let i = 0;
+    let interval = setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
+        var _a;
+        let item = positions[i];
+        yield sounds[item].play();
+        yield ((_a = parts[item]) === null || _a === void 0 ? void 0 : _a.classList.add(`genius__item--${item}__active`));
+        setTimeout(() => __awaiter(void 0, void 0, void 0, function* () {
+            var _b;
+            yield ((_b = parts[item]) === null || _b === void 0 ? void 0 : _b.classList.remove(`genius__item--${item}__active`));
+        }), 700);
+        i++;
+        if (i === positions.length) {
+            clearInterval(interval);
+        }
+    }), 1500);
+});
 const defineHeight = () => {
     const elements = document.querySelectorAll(".genius__item");
     elements.forEach((element) => {
         element.style.height = `${element.clientWidth}px`;
     });
 };
-const startGame = () => {
-    if (!jogoIniciado) {
-        alert(`Let´s Go!`);
-        jogoIniciado = true;
-        if (btnStart) {
-            btnStart.disabled = true;
-            btnStart.style.cursor = "none";
-            btnStart.style.color = "black";
-        }
+const restart = () => {
+    jogoIniciado = false;
+    if (btnStart) {
+        btnStart.disabled = false;
+        btnStart.innerHTML = "START";
+        btnStart.style.fontSize = "2em";
     }
+};
+const lost = (bootstrap, element) => {
+    const verify = verifySentence();
+    if (!verify) {
+        const modal = new bootstrap.Modal(element, {});
+        modal.show();
+    }
+};
+const verifySentence = () => {
+    return false;
 };
 defineHeight();
 window.addEventListener("resize", () => {
     defineHeight();
+});
+window.addEventListener("load", () => {
+    configs();
 });
--- genius/src/script.ts
+++ genius/src/script.ts
@@ -2,6 +2,84 @@ var jogoIniciado = false;
 
 const btnStart = document.getElementById('part-4') as HTMLButtonElement | null;
 
+const parts = [
+    document.getElementById('part-0'),
+    document.getElementById('part-1'),
+    document.getElementById('part-2'),
+    document.getElementById('part-3'),
+    document.getElementById('part-4'),
+]
+
+const sounds = [
+    new Audio('../build/assets/sounds/sound0.wav'),
+    new Audio('../build/assets/sounds/sound1.wav'),
+    new Audio('../build/assets/sounds/sound2.wav'),
+    new Audio('../build/assets/sounds/sound3.wav'),
+    new Audio('../build/assets/sounds/erro.mp3'),
+]
+
+const score = document.getElementById("genius__score");
+
+const areaeError = document.getElementById("area__error");
+
+let positions: any = [], mPositions: any = [];
+
+const configs = () => {
+    parts.map((part, idx) => {
+        part?.addEventListener('click', (event) => setPosition(event))
+    })
+}
+
+const startGame = async () => {
+    if (!jogoIniciado) {
+        jogoIniciado = true;
+        if (btnStart) {
+            btnStart.disabled = true;
+            btnStart.innerHTML = "GAME </br> STARTED";
+            btnStart.style.fontSize = "1.5em";
+            btnStart.style.cursor = "none";
+            loadPosition();
+        }
+
+    }
+    // GAME POSITION GENERATION LOGIC
+}
+
+const loadPosition = async () => {
+    let aleatory = 0;
+    if (positions.length >= 4) {
+        aleatory = Math.floor(Math.random() * 4)
+    } else {
+        if (positions.length > 0) {
+            aleatory = positions[positions.length - 1] + 1;
+        }
+    }
+    positions.push(aleatory);
+    await iluminatePosition();
+    mPositions = [];
+}
+
+const setPosition = async (event: any) => {
+    let element = event.currentTarget.getAttribute(`source`);
+
+}
+
+const iluminatePosition = async () => {
+    let i = 0;
+    let interval = setInterval(async () => {
+        let item = positions[i];
+        await sounds[item].play()
+        await parts[item]?.classList.add(`genius__item--${item}__active`)
+        setTimeout(async () => {
+            await parts[item]?.classList.remove(`genius__item--${item}__active`)
+        }, 700)
+        i++;
+        if (i === positions.length) {
+            clearInterval(interval);
+        }
+    }, 1500);
+}
+
 const defineHeight = () => {
     const elements: any = document.querySelectorAll(".genius__item");
     elements.forEach((element: any) => {
@@ -9,24 +87,41 @@ const defineHeight = () => {
     })
 }
 
-const startGame = () => {
-    if (!jogoIniciado) {
-        alert(`Let´s Go!`);
-        jogoIniciado = true;
-        if (btnStart) {
-            btnStart.disabled = true;
-            btnStart.style.cursor = "none";
-            btnStart.style.color = "black";
-        }
+
+
+
+const restart = () => {
+    jogoIniciado = false;
+    if (btnStart) {
+        btnStart.disabled = false;
+        btnStart.innerHTML = "START";
+        btnStart.style.fontSize = "2em";
     }
+}
+
+const lost = (bootstrap: any, element: HTMLElement) => {
+    const verify = verifySentence();
+    if (!verify) {
+        const modal = new bootstrap.Modal(element, {});
+        modal.show();
+    }
+}
+
+const verifySentence = () => {
+    return false;
+
 }
 
 defineHeight();
 
 window.addEventListener("resize", () => {
     defineHeight();
-})
+});
 
+
+window.addEventListener("load", () => {
+    configs();
+});
 
 
 
